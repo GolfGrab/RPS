b84dcec466b3e8f715b98ce02104a28bc795b7ae
GolfGrab <surapus01@gmail.com>
2024-02-10 15:07:37 +0700

    fix: RPS security  * CommitReveal    prevent front run by extend and use CommitReveal.sol    make CommitReveal.sol unusable directly from outside  * add external pure function for calculate Hashed Choice With Salt  * add new event to inform idx when add new player  * adjust function to input Hashed Choice and then allow to show choice  * reset game when game is finished

--- RPS.sol
+++ RPS.sol
@@ -1,54 +1,87 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+import "CommitReveal.sol";
 
-contract RPS {
+contract RPS is CommitReveal {
     struct Player {
-        uint choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
+        uint256 choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
         address addr;
     }
-    uint public numPlayer = 0;
-    uint public reward = 0;
-    mapping (uint => Player) public player;
-    uint public numInput = 0;
+    mapping(uint256 => Player) public player;  // only player id = 0 and player id = 1 only
+    uint256 public numPlayer = 0;
+    uint256 public reward = 0;
+    uint256 public numInput = 0;
+    uint256 public numShow = 0;
+
+    function getHashedChoiceWithSalt(uint256 choice, string memory salt)
+        external
+        pure
+        returns (bytes32)
+    {
+        bytes32 encodedSalt = bytes32(abi.encodePacked(salt));
+        return keccak256(abi.encodePacked(choice, encodedSalt));
+    }
 
     function addPlayer() public payable {
-        require(numPlayer < 2);
-        require(msg.value == 1 ether);
+        require(numPlayer < 2, "maximum player limit exceed");
+        require(msg.value == 1 ether, "incorrect value");
         reward += msg.value;
         player[numPlayer].addr = msg.sender;
         player[numPlayer].choice = 3;
+        emit PlayerAdded(numPlayer, msg.sender);
         numPlayer++;
     }
+    event PlayerAdded(uint256 id, address addr);
 
-    function input(uint choice, uint idx) public  {
+    function inputHashedChoice(bytes32 hashedChoiceWithSalt, uint256 idx)
+        public
+    {
+        require(numPlayer == 2, "not enough player");
+        require(msg.sender == player[idx].addr, "invelid address");
+        commit(getHash(hashedChoiceWithSalt));
+        numInput++;
+    }
+
+    function showChoice(
+        uint256 choice,
+        string memory salt,
+        uint256 idx
+    ) public {
         require(numPlayer == 2);
+        require(numInput == 2);
         require(msg.sender == player[idx].addr);
         require(choice == 0 || choice == 1 || choice == 2);
+        bytes32 encodedSalt = bytes32(abi.encodePacked(salt));
+        reveal(keccak256(abi.encodePacked(choice, encodedSalt)));
         player[idx].choice = choice;
-        numInput++;
-        if (numInput == 2) {
+        numShow++;
+        emit ChoiceRevealed(idx, choice);
+        if (numShow == 2) {
             _checkWinnerAndPay();
         }
     }
+    event ChoiceRevealed(uint256 id, uint256 choice);
 
     function _checkWinnerAndPay() private {
-        uint p0Choice = player[0].choice;
-        uint p1Choice = player[1].choice;
+        uint256 p0Choice = player[0].choice;
+        uint256 p1Choice = player[1].choice;
         address payable account0 = payable(player[0].addr);
         address payable account1 = payable(player[1].addr);
         if ((p0Choice + 1) % 3 == p1Choice) {
             // to pay player[1]
             account1.transfer(reward);
-        }
-        else if ((p1Choice + 1) % 3 == p0Choice) {
+        } else if ((p1Choice + 1) % 3 == p0Choice) {
             // to pay player[0]
-            account0.transfer(reward);    
-        }
-        else {
+            account0.transfer(reward);
+        } else {
             // to split reward
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
         }
+
+        numInput = 0;
+        numPlayer = 0;
+        numShow = 0;
     }
 }

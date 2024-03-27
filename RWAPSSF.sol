1ebe04e590c238dbeec96d022484cbb67b332916
GolfGrab <surapus01@gmail.com>
2024-03-27 17:14:59 +0700

    Update RPS game logic to include two choices per player

--- RWAPSSF.sol
+++ RWAPSSF.sol
@@ -32,7 +32,8 @@ contract RPS is CommitReveal {
     */
 
     struct Player {
-        uint64 choice; // 0-Rock 1-Water 2-Air 3-Paper 4-Sponge 5-Scissors 6-Fire 7-Unrevealed
+        uint64 choice1; // 0-Rock 1-Water 2-Air 3-Paper 4-Sponge 5-Scissors 6-Fire 7-Unrevealed
+        uint64 choice2; // 0-Rock 1-Water 2-Air 3-Paper 4-Sponge 5-Scissors 6-Fire 7-Unrevealed
         bool isCommitted;
         address addr;
     }
@@ -46,13 +47,13 @@ contract RPS is CommitReveal {
     uint256 public constant TIME_IDLE_DURATION = 1 minutes;
     uint256 public lastActionTime = block.timestamp;
 
-    function getHashedChoiceWithSalt(uint64 choice, string memory salt)
-        external
-        pure
-        returns (bytes32)
-    {
+    function getHashedChoiceWithSalt(
+        uint64 choice1,
+        uint64 choice2,
+        string memory salt
+    ) external pure returns (bytes32) {
         bytes32 encodedSalt = bytes32(abi.encodePacked(salt));
-        return keccak256(abi.encodePacked(choice, encodedSalt));
+        return keccak256(abi.encodePacked(choice1, choice2, encodedSalt));
     }
 
     function addPlayer() public payable {
@@ -60,7 +61,8 @@ contract RPS is CommitReveal {
         require(msg.value == 1 ether, "incorrect value");
         reward += msg.value;
         player[numPlayer].addr = msg.sender;
-        player[numPlayer].choice = 7; // 7 means unrevealed choice
+        player[numPlayer].choice1 = 7; // 7 means unrevealed choice
+        player[numPlayer].choice2 = 7; // 7 means unrevealed choice
         emit PlayerAdded(numPlayer, msg.sender);
         numPlayer++;
         lastActionTime = block.timestamp;
@@ -68,9 +70,10 @@ contract RPS is CommitReveal {
 
     event PlayerAdded(uint256 id, address addr);
 
-    function inputHashedChoice(bytes32 hashedChoiceWithSalt, uint256 idx)
-        public
-    {
+    function inputHashedChoice(
+        bytes32 hashedChoiceWithSalt,
+        uint256 idx
+    ) public {
         require(numPlayer == 2, "not enough player");
         require(msg.sender == player[idx].addr, "invelid address");
         require(player[idx].isCommitted == false, "already committed");
@@ -80,55 +83,94 @@ contract RPS is CommitReveal {
     }
 
     function showChoice(
-        uint64 choice,
+        uint64 choice1,
+        uint64 choice2,
         string memory salt,
         uint256 idx
     ) public {
         require(numPlayer == 2, "not enough player");
         require(numInput == 2, "not enough input");
         require(msg.sender == player[idx].addr, "invelid address");
-        require(choice >= 0 && choice <= 6, "invalid choice");
+        require(choice1 >= 0 && choice1 <= 6, "invalid choice1");
+        require(choice2 >= 0 && choice2 <= 6, "invalid choice2");
         bytes32 encodedSalt = bytes32(abi.encodePacked(salt));
-        reveal(keccak256(abi.encodePacked(choice, encodedSalt)));
-        player[idx].choice = choice;
+        reveal(keccak256(abi.encodePacked(choice1, choice2, encodedSalt)));
+        player[idx].choice1 = choice1;
+        player[idx].choice2 = choice2;
         numShow++;
-        emit ChoiceRevealed(idx, choice);
+        emit ChoiceRevealed(idx, choice1, choice2);
         lastActionTime = block.timestamp;
         if (numShow == 2) {
             _checkWinnerAndPay();
         }
     }
 
-    event ChoiceRevealed(uint256 id, uint64 choice);
+    event ChoiceRevealed(uint256 id, uint64 choice1, uint64 choice2);
 
     function _checkWinnerAndPay() private {
-        uint64 p0Choice = player[0].choice;
-        uint64 p1Choice = player[1].choice;
-        uint64 diff = (p0Choice + 7 - p1Choice) % 7;
         address payable account0 = payable(player[0].addr);
         address payable account1 = payable(player[1].addr);
         uint256 winner; // 0 - player 0 , 1 - player 1 , 2 - draw
 
-        if (diff == 0) {
+        uint64 p0Score = 0;
+        uint64 p0Choice1 = player[0].choice1;
+        uint64 p0Choice2 = player[0].choice2;
+
+        uint64 p1Score = 0;
+        uint64 p1Choice1 = player[1].choice1;
+        uint64 p1Choice2 = player[1].choice2;
+
+        // round 1
+        uint64 diff1 = (p0Choice1 + 7 - p1Choice1) % 7;
+        if (diff1 == 0) {
             // draw
-            account0.transfer(reward / 2);
-            account1.transfer(reward / 2);
-            winner = 2;
-        } else if (diff <= 3 && diff != 0) {
+            p0Score += 1;
+            p1Score += 1;
+        } else if (diff1 <= 3 && diff1 != 0) {
             // player 0 win
-            account0.transfer(reward);
-            winner = 0;
-        } else if (diff > 3 && diff != 0) {
+            p0Score += 2;
+        } else if (diff1 > 3 && diff1 != 0) {
             // player 1 win
-            account1.transfer(reward);
-            winner = 1;
+            p1Score += 2;
         }
 
-        emit Winner(winner, p0Choice, p1Choice);
+        // round 2
+        uint64 diff2 = (p0Choice2 + 7 - p1Choice2) % 7;
+        if (diff2 == 0) {
+            // draw
+            p0Score += 1;
+            p1Score += 1;
+        } else if (diff2 <= 3 && diff2 != 0) {
+            // player 0 win
+            p0Score += 2;
+        } else if (diff2 > 3 && diff2 != 0) {
+            // player 1 win
+            p1Score += 2;
+        }
+
+        if (p0Score > p1Score) {
+            winner = 0;
+            account0.transfer(reward);
+        } else if (p0Score < p1Score) {
+            winner = 1;
+            account1.transfer(reward);
+        } else {
+            winner = 2;
+            account0.transfer(reward / 2);
+            account1.transfer(reward / 2);
+        }
+
+        emit Winner(winner, p0Choice1, p1Choice1, p0Choice2, p1Choice2);
         _resetGame();
     }
 
-    event Winner(uint256 winner, uint64 p0Choice, uint64 p1Choice);
+    event Winner(
+        uint256 winner,
+        uint64 p0Choice,
+        uint64 p1Choice,
+        uint64 p0Choice2,
+        uint64 p1Choice2
+    );
 
     function _resetGame() private {
         numPlayer = 0;
@@ -183,11 +225,11 @@ contract RPS is CommitReveal {
         }
         // punish player who not revealed their choice in time
         else if (numPlayer == 2 && numShow == 1) {
-            if (player[0].choice == 7) {
+            if (player[0].choice1 == 7 || player[0].choice2 == 7) {
                 // player 0 has not revealed their choice
                 address payable account = payable(player[1].addr);
                 account.transfer(reward);
-            } else if (player[1].choice == 7) {
+            } else if (player[1].choice1 == 7 || player[1].choice2 == 7) {
                 // player 1 has not revealed their choice
                 address payable account = payable(player[0].addr);
                 account.transfer(reward);
